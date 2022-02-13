eaf9b92e3a076241afcc73c93b2b11fa9bb9bef2
Nypiaka <98625721+Nypiaka@users.noreply.github.com>
2022-02-13 14:34:29 +0300

    Delete game directory

--- game/CharTaker.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public interface CharTaker {
-    char CharEmpty = '.';
-    char firstPlayersMark = 'X';
-    char secondPlayersMark = 'O';
-}
--- game/Main.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Game TicTac = new Game();
-        System.out.println(TicTac.TicTacGame());
-    }
-}
